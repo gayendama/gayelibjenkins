bb7c047b1499e5c5270cbfc08b2cea7797543983
gayendama <117781785+gayendama@users.noreply.github.com>
2024-07-18 12:02:08 +0000

    Update buildDockerImage.groovy

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
 def buildDockerImage(String repository, String version) {
-    sh "docker build -t ${repository}:${version} ."
+    sh "docker build -t ${repository}:${IMAGE_VERSION} ."
 }
 
