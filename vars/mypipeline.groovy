ffcde9e9868ce4eb9c2c9a2f5d9eb9256e07baa6
gayendama <117781785+gayendama@users.noreply.github.com>
2024-07-18 11:02:50 +0000

    Update mypipeline.groovy

--- vars/mypipeline.groovy
+++ vars/mypipeline.groovy
@@ -3,9 +3,13 @@ def call(Map config) {
         agent any
 
         environment {
-        DOCKER_REPOSITORY = 'ndamagaye286'  // Remplacez par le nom de votre repository
-        IMAGE_VERSION = "${env.BUILD_NUMBER}"    // Utilise le numéro de build Jenkins comme version de l'image
-    }
+            DOCKER_REPOSITORY = 'ndamagaye286'  // Remplacez par le nom de votre repository
+            IMAGE_VERSION = "${env.BUILD_NUMBER}"    // Utilise le numéro de build Jenkins comme version de l'image
+            IMAGE_NAME = config.imageName ?: 'votre_image'
+            PortContainer = config.portContainer ?: '8080'
+            PortApp = config.portApp ?: '80'
+            IMAGE_TAG = config.imageTag ?: 'latest'
+        }
     stages {
         stage('Build Docker image') {
             steps {
