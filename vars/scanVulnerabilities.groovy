c58e0aa0b64c62ebc0816fbe0f9da00bdeeee5ac
gayendama <117781785+gayendama@users.noreply.github.com>
2024-07-23 13:49:56 +0000

    Update scanVulnerabilities.groovy

--- vars/scanVulnerabilities.groovy
+++ vars/scanVulnerabilities.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 def call () {
     // Scanner l'image Docker avec Trivy
-    sh "trivy image --no-progress --exit-code 1 --severity HIGH ${imageName}:${imageTag}"
+    sh "trivy image --no-progress --exit-code 1 --severity HIGH house-innovation:latest"
     sleep 10
 }
